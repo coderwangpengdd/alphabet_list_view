eb2e25f34028ebe25a1951c526e1274b32eefd85
王鹏 <peng.wang@bigmarker.com>
2025-04-21 19:18:52 +0800

    update data

--- example/lib/pages/default.dart
+++ example/lib/pages/default.dart
@@ -2,7 +2,7 @@
  * @Author: 王鹏 peng.wang@bigmarker.com
  * @Date: 2025-04-21 14:46:32
  * @LastEditors: 王鹏 peng.wang@bigmarker.com
- * @LastEditTime: 2025-04-21 16:32:13
+ * @LastEditTime: 2025-04-21 19:10:06
  * @FilePath: /example/lib/pages/default.dart
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
