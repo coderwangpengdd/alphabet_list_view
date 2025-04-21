3f28b0a4ee7672ae76da649db54a9ef0cab710b2
王鹏 <peng.wang@bigmarker.com>
2025-04-21 16:20:37 +0800

    UPDATE

--- lib/src/default/default_alphabet_list_header.dart
+++ lib/src/default/default_alphabet_list_header.dart
@@ -1,3 +1,11 @@
+/*
+ * @Author: 王鹏 peng.wang@bigmarker.com
+ * @Date: 2025-04-21 14:46:32
+ * @LastEditors: 王鹏 peng.wang@bigmarker.com
+ * @LastEditTime: 2025-04-21 16:20:10
+ * @FilePath: /example/Users/wangxiaoying/Desktop/bigmarker/alphabet_list_view/lib/src/default/default_alphabet_list_header.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -23,7 +31,6 @@ class DefaultAlphabetListHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
       padding: const EdgeInsets.fromLTRB(0, 24, 0, 8),
       width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
